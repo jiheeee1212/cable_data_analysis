44c98aeefe9dc361fe2a21cd62d8d0e760f67fdc
wndus dks <dkswndus6988@gmail.com>
2026-01-26 23:43:42 +0900

    feat: 단일컬럼 카테고리 변환

---
+++ src/schema.py
@@ -0,0 +1,17 @@
+ASSET_PROD_MAP = {
+    "FOD": 0,
+    "RVOD": 1,
+    "SVOD": 2
+}
+
+SCREEN_TP_MAP = {
+    'HD': 0,
+    'SD': 1,
+    'UHD': 2
+}
+
+PUBLCTN_MAP = {
+    "TV": 1,
+    "인터넷": 2,
+    "모바일": 4
+}
